cf8a54bf1df96a403d798ef17d23653e0e4f8ee0
asrar21 <alamsyedmohammadasrar@gmail.com>
2019-01-10 10:06:58 +0500

    reactjs

---
+++ src/components/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+  heloo(){
+    alert("hello")
+  }
+  render() {
+    return (
+      <div className="App">
+      <button >HOme</button>
+        
+      </div>
+    );
+  }
+}
+
+export default Home;
